65cf5b95042ff27dbe4a0f9ef008dc4a2324cfb5
Neal Uehara <ueharaneal@gmail.com>
2023-11-11 20:31:38 -0800

    Finished discover outline

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,35 @@
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+  return (
+    <div className="flex flex-col">
+      <div className=" flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 text-sm p-3 rounded-lg outline-none mt-5 sm:mt-0"
+        >
+          {genres.map((genre) => {
+            return (
+              <option key={genre.value} value={genre.value}>
+                {genre.title}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
