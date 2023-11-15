f6f57d5de71e94d6427ec5eb6902662f44f98a2c
Neal Uehara <ueharaneal@gmail.com>
2023-11-15 10:26:32 -0800

    Completed searchPage

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Swiper, SwiperSlide} from 'swiper/react';
-import { FreeMode, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
 
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
@@ -10,10 +10,9 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
-import 'swiper/css/autoplay';
 
 const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#013220] ${activeSong?.title === song?.title ? 'bg-[#013220]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
@@ -100,11 +99,7 @@ const TopPlay = () => {
           freeMode
           centeredSlides
           centeredSlidesBounds
-          modules={[FreeMode, Autoplay]} // Include Autoplay in the modules
-          autoplay={{
-            delay: 2500, // Delay in milliseconds between slides. Adjust as needed.
-            disableOnInteraction: false // Continue autoplay when the swiper is interacted with.
-          }}
+          modules={[FreeMode]}
           className="mt-4"
         >
           {topPlays?.slice(0, 5).map((artist) => (
@@ -124,4 +119,4 @@ const TopPlay = () => {
   );
 };
 
-export default TopPlay;   
+export default TopPlay;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,17 +2,20 @@
 import  {useDispatch, useSelector } from 'react-redux '
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { selectGenreListId } from '../redux/features/playerSlice'
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore"; 
 
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const { activeSong, isPlaying} = useSelector((state)=> state.player);
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = "Pop";
+  const { activeSong, isPlaying, genreListId} = useSelector((state)=> state.player);
+  const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP ');
+  
 
   if (isFetching) return <Loader title="Loading Songs..." />;
   if (error) return <Error />;
+
+  const genreTitle = genres.find(({ value })=> value  === genreListId)?.title
   return (
     <div className="flex flex-col">
       <div className=" flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -20,8 +23,8 @@ const Discover = () => {
           Discover {genreTitle}
         </h2>
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => {dispatch(selectGenreListId(e.target.value))}}
+          value={genreListId||"Pop"}
           className="bg-black text-gray-300 text-sm p-3 rounded-lg outline-none mt-5 sm:mt-0"
         >
           {genres.map((genre) => (
