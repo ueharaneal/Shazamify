e5be1cf51b07447d293665dbff44f614058d8737
Neal Uehara <ueharaneal@gmail.com>
2023-11-15 08:32:58 -0800

    Completed Around You

--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable jsx-a11y/media-has-caption */
+
 import React, { useRef, useEffect } from 'react';
 
 const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate, onLoadedData, repeat }) => {
   const ref = useRef(null);
-  // eslint-disable-next-line no-unused-expressions
+ 
   if (ref.current) {
     if (isPlaying) {
       ref.current.play();
