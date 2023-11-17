7478253f8a55a6976156a47ce49ef83601141059
Neal Uehara <ueharaneal@gmail.com>
2023-11-17 01:13:33 -0800

    Completed Website :)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
-import { systemDesign,ShazamifyPDF } from "../assets";
+import { systemDesign, ShazamifyPDF } from "../assets";
 
 function About() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mr-10">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-12">
         About
       </h2>
+      <h1 className="font-bold text-2xl text-gray-400 text-left mt-4 mb-4">
+        Overview:
+      </h1>
+      <p className="text-white pb-6 ml-6">
+        Shazamify is a cutting-edge music streaming web application that offers
+        a unique and interactive experience for music lovers. This web app
+        stands out with its third-party backend integration, extensive API
+        library, and visually appealing user interface.
+      </p>
       <h2 className="font-bold text-2xl text-gray-400 text-left mt-4 mb-4">
         Design Phase:
       </h2>
@@ -20,20 +29,24 @@ function About() {
           Click here to download full diagram PDF
           <FaArrowRight className="h-8 pl-1" />
         </a>
-        <img className="mx-8 mr-12" src={systemDesign}/>
+        <img className="mx-8 mr-12" src={systemDesign} />
       </div>
       <h2 className="font-bold text-2xl text-gray-400 text-left mt-4 mb-8">
         Key Features:
       </h2>
       <div className="flex flex-col text-white ml-6">
-        <h1 className="font-bold text-xl pb-5">Overview:</h1>
-        <p className="pb-12">
-          Shazamify is a cutting-edge music streaming web application that
-          offers a unique and interactive experience for music lovers. This web
-          app stands out with its third-party backend integration, extensive API
-          library, and visually appealing user interface.
-        </p>
-
+        <li>
+          <span className="font-bold text-lg">Top 50 Songs Discovery: </span>Discover the top 50 songs globally on the
+          Discover page, with an option to filter by genre. Interactive Song
+          Play: Experience a snippet of the song (1:30 seconds, due to Shazam
+          API limitations) and get recommendations for similar
+        </li>
+        <li className="my-10">
+          <span className="font-bold text-lg">Dynamic UI with Tailwind CSS and Swiper: </span>
+          Implements Swiper for smooth animations and mobile-friendly horizontal swiping.
+          Utilized Tailwind for maximizing component customization.
+        </li>
+        
         <ul className="font-bold text-xl pb-5">
           Third-Party Backend Integration:
         </ul>
@@ -48,10 +61,13 @@ function About() {
           song playing for 1:30 minutes.
         </li>
 
-        <h1 className="font-bold text-xl pb-5">
-          Engaging User Interface with Swiper:
-        </h1>
-        <p className="pb-12"></p>
+        <ul className="font-bold text-xl pb-5">
+          Redux Toolkit for State Management:
+        </ul>
+        <li className="pb-12">
+          Employs Redux Toolkit's query feature for efficient data fetching,
+          caching, and state synchronization.
+        </li>
       </div>
     </div>
   );
