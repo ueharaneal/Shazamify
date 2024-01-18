e0741deb47b08c7b09981cc96152b7d97981c4cf
Neal Uehara <ueharaneal@gmail.com>
2024-01-17 20:01:46 -0800

    Set default area code ot us in case geoAPI could not access it

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -6,17 +6,34 @@ import { Error, Loader, SongCard } from '../components';
 import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const CountryTracks = () => {
-  const [country, setCountry] = useState('');
+  // Define a default country code
+  const DEFAULT_COUNTRY_CODE = 'US';
+  
+  const [country, setCountry] = useState(DEFAULT_COUNTRY_CODE);
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
-    axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
-      .then((res) => setCountry(res?.data?.location.country))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+    if (country === DEFAULT_COUNTRY_CODE) {
+      axios
+        .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+        .then((res) => {
+          const fetchedCountry = res?.data?.location.country;
+          if (fetchedCountry) {
+            setCountry(fetchedCountry);
+          } // If fetchedCountry is undefined or empty, the state remains at default value.
+        })
+        .catch((err) => {
+          console.log(err);
+          // Optionally handle the error, e.g., show an error message.
+          // The country state will remain at the default value.
+        })
+        .finally(() => setLoading(false));
+    } else {
+      // If the country is not the default (i.e., it's already set), we don't need to load again.
+      setLoading(false);
+    }
   }, [country]);
 
   if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
@@ -44,3 +61,4 @@ const CountryTracks = () => {
 };
 
 export default CountryTracks;
+
